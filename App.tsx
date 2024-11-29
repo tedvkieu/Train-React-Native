669802d9e621dd6ef09a54ab067b7df870b14c4e
tedvkieu <dvkieu165@gmail.com>
2024-11-29 12:28:36 +0700

    #8. Flat List

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {
     Button,
+    FlatList,
     ScrollView,
     StyleSheet,
     Text,
@@ -20,18 +21,35 @@ export default function App() {
         { id: 6, name: 'Kieu6', age: 18 },
         { id: 7, name: 'Kieu7', age: 18 },
         { id: 8, name: 'Kieu8', age: 18 },
-        { id: 9, name: 'Kieu8', age: 18 },
-        { id: 10, name: 'Kieu8', age: 18 },
-        { id: 11, name: 'Kieu8', age: 18 },
-        { id: 11, name: 'Kieu8', age: 18 },
-        { id: 11, name: 'Kieu8', age: 18 },
+        { id: 9, name: 'Kieu9', age: 18 },
+        { id: 10, name: 'Kieu10', age: 18 },
+        { id: 12, name: 'Kieu11', age: 18 },
+        { id: 13, name: 'Kieu12', age: 18 },
+        { id: 14, name: 'Kieu13', age: 18 },
     ]);
 
     return (
         // view tuong ung voi the div
         <View style={styles.container}>
-            <Text style={{ fontSize: 60 }}>Hello World</Text>
-            <ScrollView>
+            <Text style={{ fontSize: 60 }}>Kiue ne</Text>
+            <FlatList
+                data={students}
+                keyExtractor={(item) => item.id + ''}
+                renderItem={(data) => {
+                    return (
+                        <View
+                            style={{
+                                padding: 30,
+                                backgroundColor: 'pink',
+                                marginBottom: 30,
+                                marginHorizontal: 40,
+                            }}>
+                            <Text>{data.item.name}</Text>
+                        </View>
+                    );
+                }}
+            />
+            {/* <ScrollView>
                 {students.map((item) => {
                     return (
                         <View
@@ -45,7 +63,7 @@ export default function App() {
                         </View>
                     );
                 })}
-            </ScrollView>
+            </ScrollView> */}
         </View>
     );
 }
