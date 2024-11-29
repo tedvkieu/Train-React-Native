30a38e421ce87e725397d3e10529b9476239d1da
tedvkieu <dvkieu165@gmail.com>
2024-11-29 12:59:30 +0700

    #9. Todo App - Part 1

--- App.tsx
+++ App.tsx
@@ -9,72 +9,82 @@ import {
     View,
 } from 'react-native';
 
+interface ITodo {
+    id: number;
+    name: string;
+}
+
 export default function App() {
     // jsx react native
 
-    const [students, setStudents] = useState([
-        { id: 1, name: 'Kieu1', age: 18 },
-        { id: 2, name: 'Kieu2', age: 18 },
-        { id: 3, name: 'Kieu3', age: 18 },
-        { id: 4, name: 'Kieu4', age: 18 },
-        { id: 5, name: 'Kieu5', age: 18 },
-        { id: 6, name: 'Kieu6', age: 18 },
-        { id: 7, name: 'Kieu7', age: 18 },
-        { id: 8, name: 'Kieu8', age: 18 },
-        { id: 9, name: 'Kieu9', age: 18 },
-        { id: 10, name: 'Kieu10', age: 18 },
-        { id: 12, name: 'Kieu11', age: 18 },
-        { id: 13, name: 'Kieu12', age: 18 },
-        { id: 14, name: 'Kieu13', age: 18 },
-    ]);
+    const [todo, setTodo] = useState('');
+    const [listTodo, setListTodo] = useState<ITodo[]>([]);
 
+    function randomInteger(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    const handleAddTodo = () => {
+        if (!todo) return;
+        setListTodo([...listTodo, { id: randomInteger(2, 2000), name: todo }]);
+        setTodo('');
+    };
     return (
         // view tuong ung voi the div
         <View style={styles.container}>
-            <Text style={{ fontSize: 60 }}>Kiue ne</Text>
-            <FlatList
-                data={students}
-                keyExtractor={(item) => item.id + ''}
-                renderItem={(data) => {
-                    return (
-                        <View
-                            style={{
-                                padding: 30,
-                                backgroundColor: 'pink',
-                                marginBottom: 30,
-                                marginHorizontal: 40,
-                            }}>
-                            <Text>{data.item.name}</Text>
-                        </View>
-                    );
-                }}
-            />
-            {/* <ScrollView>
-                {students.map((item) => {
-                    return (
-                        <View
-                            key={item.id}
-                            style={{
-                                padding: 30,
-                                backgroundColor: 'pink',
-                                marginBottom: 30,
-                            }}>
-                            <Text>{item.name}</Text>
-                        </View>
-                    );
-                })}
-            </ScrollView> */}
+            <Text style={styles.header}>Todo App</Text>
+
+            <View style={styles.body}>
+                <TextInput
+                    value={todo}
+                    style={styles.input}
+                    onChangeText={(value) => setTodo(value)}
+                />
+                <Button title="ADD to do" onPress={() => handleAddTodo()} />
+            </View>
+            <View style={styles.body}>
+                <FlatList
+                    keyExtractor={(item) => item.id + ''}
+                    data={listTodo}
+                    renderItem={({ item }) => {
+                        return <Text style={styles.todoItem}>{item.name}</Text>;
+                    }}
+                />
+            </View>
         </View>
     );
 }
 
 //ko co css, stylesheet giong css
 const styles = StyleSheet.create({
+    header: {
+        backgroundColor: 'orange',
+        paddingHorizontal: 20,
+        textAlign: 'center',
+        fontSize: 50,
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: 'green',
+        padding: 5,
+        marginHorizontal: 15,
+        margin: 15,
+    },
     container: {
         paddingTop: 50,
         flex: 1,
         backgroundColor: '#fff',
         // alignItems: 'center',
         // justifyContent: 'center',
+    },
+    todoItem: {
+        fontSize: 20,
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        marginBottom: 20,
+        padding: 10,
+    },
+    body: {
+        paddingHorizontal: 10,
+        marginBottom: 20,
     },
 });
